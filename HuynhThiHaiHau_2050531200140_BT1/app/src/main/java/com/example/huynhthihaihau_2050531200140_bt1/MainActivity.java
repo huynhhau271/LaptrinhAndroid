d5b2e1255f4413ee618617b0a1c28bf795e55e7c
Hau <huynhhau2580@gmail.com>
2022-09-18 21:27:54 +0700

    commit tao link git moi

---
+++ HuynhThiHaiHau_2050531200140_BT1/app/src/main/java/com/example/huynhthihaihau_2050531200140_bt1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.huynhthihaihau_2050531200140_bt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
