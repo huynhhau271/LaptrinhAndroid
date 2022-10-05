29d2f0496d45f21f52db6c7806a42b43e9de18ca
Hau <huynhhau2580@gmail.com>
2022-10-05 13:49:06 +0700

    commit bai tap

--- HuynhThiHaiHau_LT_BT6_SharePreference/app/src/main/java/com/example/huynhthihaihau_lt_bt6_sharepreference/MainActivity.java
+++ HuynhThiHaiHau_LT_BT6_SharePreference/app/src/main/java/com/example/huynhthihaihau_lt_bt6_sharepreference/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,8 +17,9 @@ import com.google.android.material.button.MaterialButton;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    MaterialButton btXacNhan;
-
+    Button logninbtn;
+    EditText edtUsername, edtPassword;
+    CheckBox cbRemember;
     SharedPreferences sharedPreferences;
 
 
@@ -24,32 +27,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView username = (TextView) findViewById(R.id.userName);
-        TextView password = (TextView) findViewById(R.id.passWord);
-        MaterialButton signup = (MaterialButton) findViewById(R.id.signupbt);
-        setContentView(R.layout.activity_main);
+        sharedPreferences = getSharedPreferences("dataLogin", MODE_PRIVATE);
 
 
-        signup.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-
-                Toast.makeText(MainActivity.this, "PROFILE OPEN!", Toast.LENGTH_SHORT).show();
-                //doOpenSignupActivity();
-
-            }
-        });
+        AnhXa();
+        //Lấy giá trị
+        edtUsername.setText(sharedPreferences.getString("TaiKhoan",""));
+        edtPassword.setText(sharedPreferences.getString("MatKhau",""));
+        cbRemember.setChecked(sharedPreferences.getBoolean("Checked", false));
 
 
-
-        MaterialButton logninbtn = (MaterialButton) findViewById(R.id.loginbtn);
         logninbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                String edtusername = edtUsername.getText().toString().trim();
+                String edtpassword = edtPassword.getText().toString().trim();
+                if (edtUsername.getText().toString().equals("admin") && edtPassword.getText().toString().equals("admin")) {
                     //thông báo đăng nhập thành công
                     Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                    //nếu có check
+                    if(cbRemember.isChecked()){
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("TaiKhoan", edtusername);
+                        editor.putString("MatKhau", edtpassword);
+                        editor.putBoolean("Checked",true);
+                        editor.commit();
+                    }else{
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.remove("TaiKhoan");
+                        editor.remove("MatKhau");
+                        editor.remove("Checked");
+                        editor.commit();
+                    }
+
                     //doOpenMainActivity();
                 } else
                     //thông báo đăng nhập thất bại
@@ -58,7 +68,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void AnhXa() {
+        logninbtn = (Button) findViewById(R.id.btlogin);
+        edtUsername = (EditText) findViewById(R.id.userName);
+        edtPassword = (EditText) findViewById(R.id.passWord);
+        cbRemember = (CheckBox) findViewById(R.id.cbRemember);
 
+    }
 //    public void doOpenMainActivity()
 //    {
 //        Intent myIntent=new Intent(this, MainActivity.class);
