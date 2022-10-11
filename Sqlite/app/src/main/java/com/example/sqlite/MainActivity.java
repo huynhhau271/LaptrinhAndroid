904ef13259afe1f17c4dd6f7d3a9434c507edc33
Hau <huynhhau2580@gmail.com>
2022-10-11 21:41:11 +0700

    commit bài tập

---
+++ Sqlite/app/src/main/java/com/example/sqlite/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+import com.example.sqlite.pkSqlite.DBHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DBHelper dbHelper = new DBHelper(this);
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        database.close();
+
+
+
+    }
+}
---
+++ Sqlite/app/src/main/java/com/example/sqlite/pkSqlite/EmployeeDao.java
@@ -0,0 +1,77 @@
+package com.example.sqlite.pkSqlite;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.sqlite.pkModel.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeDao {
+    private SQLiteDatabase db;
+
+    public EmployeeDao(Context context) {
+        DBHelper helper = new DBHelper(context);
+        db = helper.getWritableDatabase();
+
+    }
+    @SuppressLint("Range")
+    public List<Employee> get(String sql, String ...selectArgs) {
+        List<Employee> list = new ArrayList<>();
+        Cursor cursor = db.rawQuery(sql, selectArgs);
+        while (cursor.moveToNext()) {
+            Employee emp = new Employee();
+            emp.setId(cursor.getString(cursor.getColumnIndex("id")));
+            emp.setName(cursor.getString(cursor.getColumnIndex("name")));
+            emp.setSalary(cursor.getFloat(cursor.getColumnIndex("salary")));
+
+            list.add(emp);
+
+        }
+        return list;
+
+    }
+    public List<Employee> getAll(){
+        String sql = "SELECT * FROM nhanvien";
+
+        return get(sql);
+
+    }
+    public  Employee getById(String id){
+        String sql = "SELECT * FROM nhanvien WHERE id = ?";
+        List<Employee> list = get(sql, id);
+
+        return list.get(0);
+
+    }
+
+    //chèn thông tin của 1 nhân viên dưới dạng đối số
+    public long insert(Employee emp){
+        ContentValues values = new ContentValues();
+        values.put("id", emp.getId());
+        values.put("name", emp.getName());
+        values.put("salary", emp.getSalary());
+
+        //gọi thực hiện phương thức insert
+        return db.insert("nhanvien", null, values);
+
+    }
+
+    //cập nhận thông tin của 1 nhân viên dưới dạng đối số
+    public long update(Employee emp){
+        ContentValues values = new ContentValues();
+        values.put("name", emp.getName());
+        values.put("salary", emp.getSalary());
+
+        //gọi thực hiện phương thức update
+        return db.update("nhanvien",values, "id=?", new String[]{emp.getId()});
+
+    }
+    public int delete(String id){
+        return db.delete("nhanvien", "id=?", new String[]{id});
+    }
+}
